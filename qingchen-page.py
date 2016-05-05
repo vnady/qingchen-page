98275be271cc8bbda317ccc75dc45b6ec42c62d5
jiangtao <taojiang0101@gmail.com>
2016-05-05 16:35:26 +0800

    test return static html page

--- qingchen-page.py
+++ qingchen-page.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/templates/')
 def hello_world():
-    return app.send_static_file('/templates/index.html')
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
